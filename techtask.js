364c9064d1a90dbf1e04f79a52a59c6f7d75f0b2
Michael-Gip <gipsman@gmail.com>
2019-11-12 23:42:10 +0200

    final

--- techtask.js
+++ techtask.js
@@ -1,151 +1,161 @@
 /* New statment block creation */
-const statementBlock = (function () {
+const newBlock = (function () {
   // html structure creation
-  function blockCreat(container, editEl, dataPoint, delBut, content) {
-    const block = document.createElement(container),
-    elForContent = document.createElement(editEl),
-      delButton = document.createElement(delBut);
-
-      elForContent.setAttribute("contenteditable", "true");
-      elForContent.setAttribute("data-point", dataPoint);
+  function toCreateBlock(block, editable, toDelete, keyWord, content) {
+    const wrapper = document.createElement(block),
+      editEl = document.createElement(editable),
+      delBut = document.createElement(toDelete);
+    // Add arguments and events
+    editEl.setAttribute("contenteditable", "true");
+    editEl.setAttribute("data-point", keyWord);
+    editEl.addEventListener("click", delDefaultText);
+    editEl.addEventListener("blur", storeContent);
     if (content === undefined) {
-      elForContent.innerHTML = "Написать новое утверждение";
+      editEl.innerHTML = "Write new statement";
     } else {
-      elForContent.innerHTML = content;
+      editEl.innerHTML = content;
     }
-    elForContent.addEventListener("click", delDefaultText);
-    elForContent.addEventListener("blur", contentStoring);
-    delButton.innerHTML = "x";
-    delButton.addEventListener("click", delStatementBlock)
-    block.appendChild(elForContent);
-    block.appendChild(delButton);
-
-    return block;
+    delBut.innerHTML = "x";
+    delBut.addEventListener("click", blockToDelete);
+    wrapper.appendChild(editEl);
+    wrapper.appendChild(delBut);
+    return wrapper;
   }
-  // To search appropriate key for new element
-  function appropKey(container, editEl, delBut) {
-    // Get access to statment container
-    const eventPlace = event.target,
-      keyWord = eventPlace.getAttribute("data-containerPurpose"),
-      section = eventPlace.closest(".layer"),
-      statContain = section.querySelector(".layerDescription");
-    let index = 1;
-    // Get all created statments keys
-    const editEls = statContain.querySelectorAll("[data-point]");
-    if (editEls.length !== 0) {
-      const keys = [];
-      editEls.forEach(function (item, index) {
-        keys[index] = item.getAttribute("data-point");
+  //To search appropriate key for new statement
+  function getAppropKey(event) {
+    const eventEl = event.target,
+      keyWord = eventEl.getAttribute("data-containerPurpose")
+    let keyIndex = 1;
+    allExistKeys = [];
+    // Get access to statement container
+    statementContain = getContainer(event);
+    // Get all created statements keys
+    const createdStatements = statementContain.querySelectorAll("[data-point]");
+    if (createdStatements.length !== 0) {
+      createdStatements.forEach(function (item, index) {
+        allExistKeys[index] = item.getAttribute("data-point");
       });
-      // Loop through array to find which certain keyword with certain index still isn't there
-      while (keys.includes(keyWord + "_" + index)) {
+      // Loop through collection to find which certain keyword with certain index still isn't
+      while (allExistKeys.includes(keyWord + "_" + keyIndex)) {
+        keyIndex++;
+      }
+    }
+    return keyWord + "_" + keyIndex;
+  }
+  // To get all keys with certain keyword from localStorage
+  function getAppropKeys(keyWord) {
+    const identifier = RegExp(keyWord),
+      identifIndexes = [];
+    let index = 0;
+    for (key in localStorage) {
+      if (localStorage.hasOwnProperty(key) && identifier.test(key)) {
+        identifIndexes[index] = Number(/\d+/.exec(key));
         index++;
       }
-      // using of private method to create new statment block and output it to page
-      const dataPoint = keyWord + "_" + index;
-      const newStatBlock = blockCreat(container, editEl, dataPoint, delBut);
-      statContain.appendChild(newStatBlock);
-    } else {
-      const dataPoint = keyWord + "_" + index;
-      const newStatBlock = blockCreat(container, editEl, dataPoint, delBut);
-      statContain.appendChild(newStatBlock);
     }
+    identifIndexes.sort();
+    return identifIndexes;
+  }
+  // To get access to necessary container
+  function getContainer(accessToButton) {
+    if (typeof accessToButton === "object") {
+      var currentBut = accessToButton.target;
+    } else {
+      var currentBut = document.querySelector("[data-containerPurpose = " + accessToButton + "]");
+    }
+    currentSection = currentBut.closest(".layer"),
+      container = currentSection.querySelector(".layerDescription");
+    return container;
   }
   return {
-    blockCreat: blockCreat,
-    appropKey: appropKey
+    toCreateBlock: toCreateBlock,
+    getAppropKey: getAppropKey,
+    getAppropKeys: getAppropKeys,
+    getContainer: getContainer
   }
 })();
 
-// For work with localStorage
-const storExtractKey = (function () {
-  function setKey(event) {
-    const key = (event.target).getAttribute("data-point"),
-      content = (event.target).innerHTML;
-    localStorage.setItem(key, content);
-  }
-  function getIndexes(keyWord) {
-    // Extract all indexes which are in localStorage with a specific keyWord
-    const indexes = [];
-      let currentCustomStr = new RegExp(keyWord);
-      for (let key in localStorage) {
-        if (localStorage.hasOwnProperty(key)) {
-          if (currentCustomStr.test(key)) {
-            indexes.push(Number(/\d+/.exec(key)));
-          }
-        }
-      }
-    indexes.sort(function(a, b) {
-      return a-b;
-    });
-    return indexes;
-  }
-  return {
-    setKey: setKey,
-    getIndexes: getIndexes
-  }
-})();
-
-// event listener to add a new statement
-const createBut = document.querySelectorAll(".newStatement");
-createBut.forEach(function (item) {
-  item.addEventListener("click", createPoint);
+/* event adding a new statement */
+const creatingButs = document.querySelectorAll(".newStatement");
+creatingButs.forEach(function (item) {
+  item.addEventListener("click", addNewBlock);
 });
 
-function createPoint(event) {
-  event.preventDefault();
-  statementBlock.appropKey("p", "span", "button");
+function addNewBlock(event) {
+  const key = newBlock.getAppropKey(event),
+    block = newBlock.toCreateBlock("p", "span", "button", key);
+  // To find current container
+  container = newBlock.getContainer(event);
+  container.appendChild(block);
 }
-// To delete default text from statement block
+
+/* To delete default text from statement block */
 function delDefaultText(event) {
-  if ((event.target).innerHTML === "Написать новое утверждение") {
-    (event.target).innerHTML = "";
+  event.target.innerHTML = "";
+}
+
+/* For work with localStorage */
+const workWithStorage = (function () {
+  // Store key/content in localStorage
+  function storeInLocal(event) {
+    const editEl = event.target,
+      key = editEl.getAttribute("data-point"),
+      content = editEl.innerHTML;
+    localStorage.setItem(key, content);
   }
-}
-// To store data in localStorage
-function contentStoring(event) {
-  storExtractKey.setKey(event);
+  // Extract all indexes which are in localStorage with a specific keyWord
+  return {
+    storeInLocal: storeInLocal
+  }
+})();
+
+function storeContent(event) {
+  workWithStorage.storeInLocal(event);
 }
 
-// To delete statement block
-function delStatementBlock(event) {
+/* To delete statement block */
+function blockToDelete(event) {
   //Get and remove localStorage key
-  const block = (event.target).parentNode,
-    editEl = block.querySelector("[data-point]"),
-    key = editEl.getAttribute("data-point");
-  localStorage.removeItem(key);
+  const wrapperBlock = (event.target).closest("p"),
+    editEl = wrapperBlock.querySelector("[data-point]"),
+    theKey = editEl.getAttribute("data-point");
+  localStorage.removeItem(theKey);
   //Remove block itself
-  block.remove();
+  wrapperBlock.remove();
 }
 
-// Page state re-creation
+/* Page state re-creation */
 (function () {
-  //To get values of all possible keys for localStorage
-  const creationButs = document.querySelectorAll("[data-containerPurpose]"),
-  keysArray = [];
-  creationButs.forEach(function (item, index) {
-    keysArray[index] = item.getAttribute("data-containerPurpose");
-  });
-  // Recreation of all statment block with certain keyWord
-  keysArray.forEach(function(key, index) {
-    // To find the certain statement container
-    const theSection = creationButs[index].closest(".layer"),
-    theContainer = theSection.querySelector(".layerDescription");
-    // Recreate all statements
-    const indexes = storExtractKey.getIndexes(key);
-    if (indexes.length !== 0) {
-      indexes.forEach(function(item, index) {
-        const content = localStorage.getItem(key + "_" + item);
-        const block = statementBlock.blockCreat("p", "span", key + "_" + item,"button", content);
-        theContainer.appendChild(block);
+  // Extract all indexis of certain string identifier
+  const creatingButs = document.querySelectorAll(".newStatement");
+  creatingButs.forEach(function (item, index) {
+    const keyWord = item.getAttribute("data-containerPurpose");
+    const allIndexes = newBlock.getAppropKeys(keyWord);
+    // Re-create existing statements
+    if (allIndexes.length !== 0) {
+      allIndexes.forEach(function (item, index) {
+        const key = keyWord + "_" + allIndexes[index],
+          content = localStorage.getItem(key),
+          container = newBlock.getContainer(keyWord),
+          block = newBlock.toCreateBlock("p", "span", "button", key, content);
+        container.appendChild(block);
       });
     }
   });
 })();
 
 
-/* TODO: To look logice of exracting indexes and then using them
 
-Read Stefanof modules and write down principles
+// Recreation of all statment block with certain keyWord
+
+// To find the certain statement container
+
+// Recreate all statements
+
+
+/* TODO: 
+Build method for searching container in newBlock module. About 122, 123 lines to call method for container searching.
+module with method for search container
+To look logice of exracting indexes and then using them
 
  */
