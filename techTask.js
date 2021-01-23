0c6dfc449e24d170869934ab7ef9e7448ee849be
Michael-Gip <gipsman@gmail.com>
2021-01-24 01:01:58 +0200

    beginning structure

---
+++ techTask.js
@@ -0,0 +1,18 @@
+class DescriptionsModule {
+  save(key, description) {
+
+  }
+  delete(key)
+}
+
+// View of section elements, statements
+class sectionItemView {
+  constructor() {
+    
+  }
+}
+
+// Application logic
+class Controller {
+  
+}
